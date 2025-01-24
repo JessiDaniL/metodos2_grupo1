e851b9ed1b1d32197947f12083af225f02931e80
Valentina Rincón <123614238+ValentinaRincon@users.noreply.github.com>
2025-01-24 11:31:54 -0500

    Inicio punto 2

---
+++ Talleres/Taller_1/Punto_2/Punto2.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Leer el archivo de datos
+data = pd.read_csv('Talleres\Taller_1\Punto_2\Data\hysterisis.dat')
+
+print(data)
