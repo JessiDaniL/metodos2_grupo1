adbc15197b8c3c6e674728550861ced8eed5de7b
Juan Sebastian Pabon Martinez <sebastian252005pabon@gmail.com>
2025-01-25 10:46:45 -0500

    Finalización del punto 1, con archivo py y creacion archivos pdf

--- Talleres/Taller_1/Punto_1/Punto1_.py
+++ Talleres/Taller_1/Punto_1/Punto1_.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np 
 from scipy.optimize import curve_fit
+from scipy.integrate import simpson
+
 #cargamos y guardamos los datos de intensidad y longitud de onda 
 data = pd.read_csv(r"C:\Users\sebas\OneDrive\Documentos\Metodos_computacionales2\metodos2_grupo1\Talleres\Taller_1\Punto_1\Data\Rhodium.csv")
 lambda_=data["Wavelength (pm)"].tolist()
@@ -21,11 +23,12 @@ def filtrar(x,y,tolerancia=0.0060):
             x_nuevo.append(x[i])
             y_nuevo.append(y[i])
     datos_elimininados = len(x)-len(x_nuevo)
-    return x_nuevo,y_nuevo, print(f"1.a): Datos eliminados:{datos_elimininados}")
+    return x_nuevo,y_nuevo, f"1.a): Datos eliminados:{datos_elimininados}"
 
 lambda_filtro,intensidad_filtro,D_eliminado= filtrar(lambda_,intensidad)
 lambda_filtro=np.array(lambda_filtro)
 intensidad_filtro = np.array(intensidad_filtro)
+print(D_eliminado)
 
 #Graficamos la comparación entre los datos limpiados y los datos originales 
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 8))
@@ -69,7 +72,7 @@ parametros, _ = curve_fit(gaussianas, lambda_fondo, intensidad_fondo, p0=p0)
 ajuste_fondo= gaussianas(lambda_filtro, *parametros)
 intensidad_sin_fondo= intensidad_filtro-ajuste_fondo
 
-fig = plt.subplots(2, 1, figsize=(8, 8))
+fig = plt.subplots(figsize=(8, 8))
 plt.plot(lambda_filtro,intensidad_filtro,label="Datos filtrados")
 plt.plot(lambda_filtro,ajuste_fondo,label="Ajuste datos de fondo")
 plt.plot(lambda_filtro,intensidad_sin_fondo,label="Aislamiento de picos")
@@ -84,3 +87,76 @@ plt.show()
 print(f"1.b) Método: Ajuste gaussiano al fondo y resta con los datos filtrados ")
 
 #Punto 1.c
+def maximo_FWHM(x_, y_, region_nombre, unidades_x="nm", unidades_y="mJy"):
+
+    x = pd.Series(x_)
+    y = pd.Series(y_)
+    
+    posicion = y.idxmax()
+    coordenada_x = float(x[posicion])
+    coordenada_y = float(y[posicion])
+    
+    mitad = coordenada_y / 2
+    candidatos = []
+
+    n = len(y_)
+    for i in range(n):
+        dato_estudio = y_[i]
+        if dato_estudio >= mitad:
+            candidatos.append(i)
+    
+    x_inicial = float(x[candidatos[0]])
+    x_final = float(x[candidatos[-1]])
+    FWHM = x_final - x_inicial
+
+    return (f"1.c) El máximo local de la región '{region_nombre}' se encuentra en ({coordenada_x:.4g} {unidades_x}, "
+            f"{coordenada_y:.4g} {unidades_y}), y el ancho a media altura (FWHM) es {FWHM:.4g} {unidades_x}.")
+
+#Categoriemos el pico1
+corte_pico1 = ( (lambda_filtro> 70) & (lambda_filtro < 90)
+)
+
+intensidad_pico1= intensidad_sin_fondo[corte_pico1]
+lambda_pico1=lambda_filtro[corte_pico1]
+
+#Categorisemos el pico 2
+corte_pico2 = ( (lambda_filtro> 90) & (lambda_filtro < 110)
+)
+
+intensidad_pico2= intensidad_sin_fondo[corte_pico2]
+lambda_pico2=lambda_filtro[corte_pico2]
+
+
+
+fondo_resultado = maximo_FWHM(lambda_filtro, ajuste_fondo, "fondo")
+pico1_resultado = maximo_FWHM(lambda_pico1, intensidad_pico1, "pico 1")
+pico2_resultado = maximo_FWHM(lambda_pico2,intensidad_pico2,"pico 2")
+
+print(fondo_resultado)
+print(pico1_resultado)
+print(pico2_resultado)
+
+#Punto 1.d
+def energia (x,y):
+    energia_irradiada = simpson(y=y,x=x)
+    energia_irradiada = float(energia_irradiada)
+    energia_irradiada
+
+    n = len(y)
+
+    incertidumbres_absolutas = []
+    sumatoria_de_cuadrados = 0
+
+    for i in range (n):
+        dato_estudio = y[i]
+        i_a = dato_estudio*0.02
+        incertidumbres_absolutas.append(i_a)
+
+    for j in range (n):
+        dato_e = incertidumbres_absolutas[j]
+        cuadrado = dato_e**2
+        sumatoria_de_cuadrados += cuadrado
+    incertidumbre_total = float(np.sqrt(sumatoria_de_cuadrados))
+    return print(f"1.d) La energía total radiada equivale a: {energia_irradiada:.4g} Joules +/- {incertidumbre_total:.1g} Joules.")
+
+print(energia(lambda_filtro,intensidad_filtro))
