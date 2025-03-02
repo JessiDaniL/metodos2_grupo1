064ba6b27e3925bc155267f161ddbc6d0101ee99
Valentina Rincón <123614238+ValentinaRincon@users.noreply.github.com>
2025-03-01 20:18:23 -0500

    Inicio taller b

---
+++ Talleres/Taller_3/Taller 3b.py
@@ -0,0 +1,8 @@
+#Librerias
+import numpy as np 
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+from numba import njit
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.animation as animation
+from matplotlib.backends.backend_pdf import PdfPages
